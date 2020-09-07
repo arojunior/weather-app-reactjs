85a1342d43526eef7c0600118f45a25090a02a6a
Junior Oliveira <arojunior@gmail.com>
2020-09-07 17:30:37 +0100

    Remove boilerplate sample files

---
+++ src/components/HomeContainer.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import HomeComponent from './HomeComponent';
+
+const HomeContainer = () => <HomeComponent />
+
+export default HomeContainer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HomeContainer from './components/HomeContainer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HomeContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
